dbb2b8b228c07311aead3be4f236a8e99b55b19f
jayken.xie <jaykenxie@outlook.com>
2021-07-18 11:08:46 +0800

    init

---
+++ dao/redis/redis.go
@@ -0,0 +1,31 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"new-bell/settings"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var rdb *redis.Client
+
+func Init(conf *settings.Redis) (err error) {
+	addr := fmt.Sprintf("%s:%v", conf.Host, conf.Port)
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: conf.Passwd,
+		DB:       0,             // use default DB
+		PoolSize: conf.Poolsize, // 连接池大小
+	})
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = rdb.Ping(ctx).Result()
+	return err
+}
+
+func Close() {
+	rdb.Close()
+}
