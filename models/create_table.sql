c80cd1cb8307247631048e8a65e202954b11180a
jianjun.xie <xiejianjun@codemao.cn>
2021-09-24 18:13:39 +0800

    feat: add category table

--- models/create_table.sql
+++ models/create_table.sql
@@ -10,3 +10,10 @@ CREATE TABLE `user`(
   UNIQUE KEY `idx_user_name` (`user_name`) USING BTREE,
   UNIQUE KEY `idx_uid` (`uid`) USING BTREE
 ) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `category`(
+    `cid` int not null AUTO_INCREMENT,
+    `c_name` varchar(64) not null,
+    primary key (`cid`),
+    unique key idx_cid (`cid`) using btree
+) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
